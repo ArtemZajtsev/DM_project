4374de37e27db3ece302ad2911a526ee676e1213
rybachello <mykolarybachello@gmail.com>
2017-05-22 13:20:20 +0300

    add departure and arrival delay density distribution plot

--- project.R
+++ project.R
@@ -1,3 +1,4 @@
+#Download and cleaning data
 data <- read.csv("./flights.csv")
 
 cleanedData <- data[data$CANCELLED != 1,]
@@ -22,3 +23,39 @@ airportWithMoreThen20Flights <- airportFlightsCounter(uniqueAirports)
 library(dplyr)
 
 cleanedData <- filter(cleanedData, cleanedData$ORIGIN_AIRPORT %in% airportWithMoreThen20Flights | cleanedData$DESTINATION_AIRPORT %in% airportWithMoreThen20Flights)
+
+write.csv(cleanedData,file='cleanedData.csv',quote = FALSE,row.names = FALSE)
+
+# Plots 
+library(ggplot2)
+
+#Departure Delay Histrogram density ~ min 
+
+ggplot(data=cleanedData, aes(x= cleanedData$DEPARTURE_DELAY, y = ..density..)) + 
+    geom_histogram(
+                   binwidth = 3,
+                   col="blue", 
+                   fill="darkblue", 
+                   alpha = .7) + 
+    labs(title="2015 Departure Delay Distribution") +
+    labs(x="Min", y="Density") +
+    xlim(c(-20,130)) +
+    ylim(c(0,0.08))+
+    theme_bw() +
+    theme(plot.title = element_text(hjust = 0.5))
+
+#Arrival Delay Histrogram density ~ min 
+
+ggplot(data=cleanedData, aes(x= cleanedData$ARRIVAL_DELAY, y = ..density..)) + 
+  geom_histogram(
+    binwidth = 3,
+    col="goldenrod", 
+    fill="goldenrod1", 
+    alpha = .7) + 
+  labs(title="2015 Arrival Delay Distribution") +
+  labs(x="Min", y="Density") +
+  xlim(c(-20,130)) +
+  ylim(c(0,0.04))+
+  theme_bw() +
+  theme(plot.title = element_text(hjust = 0.5))
+
