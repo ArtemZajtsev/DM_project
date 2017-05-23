614a5b78a90d5d396f0e26db85505f5d79e87b99
Artem Zaitsev <minhiriath94@gmail.com>
2017-05-24 01:58:49 +0300

    data exploration implementation part 1

--- project.R
+++ project.R
@@ -72,7 +72,8 @@ ggplot(aggregated_airports, aes(MONTH , DEPARTURE_DELAY, colour= ORIGIN_AIRPORT)
  theme_bw() +
  ggtitle("Average delay in origin airports")+
 labs(x="Month", y="Departure delay (min)") +
-  scale_x_continuous(breaks=seq(1.0,12.0,by=1),labels=month.abb)
+  scale_x_continuous(breaks=seq(1.0,12.0,by=1),labels=month.abb)+
+  theme(plot.title = element_text(hjust = 0.5))
 
 #Plot 2b. Avg delay - DESTINATION airports (4 big airports)
 
@@ -86,7 +87,7 @@ ggplot(aggregated_airports, aes(MONTH , ARRIVAL_DELAY, colour= DESTINATION_AIRPO
   geom_line(size=2) +
   theme_bw() +
   ggtitle("Average delay in destination airports")+
-  labs( y="Arrival delay (min)")+
+  labs(x="Month", y="Arrival delay (min)")+
   scale_x_continuous(breaks=seq(1.0,12.0,by=1),labels=month.abb) 
 
 #Plot 3a.  Month - delay ORIGIN
@@ -94,12 +95,13 @@ ggplot(aggregated_airports, aes(MONTH , ARRIVAL_DELAY, colour= DESTINATION_AIRPO
 aggregated_by_month = aggregate(DEPARTURE_DELAY ~ MONTH, cleanedData, mean)
 
 plot3a = ggplot(aggregated_by_month, aes(as.factor(MONTH) , DEPARTURE_DELAY)) +
-  geom_col(size=2,fill = "blue") +
+  geom_col(size=1, col="blue",fill = " dark blue", alpha = .7) +
   theme_bw() +
   ylim(c(-1,15))+
-  ggtitle("Average delay in origin airports")+
+  ggtitle("Average departure delay by month")+
   labs(x="Month", y="Departure delay (min)") +
-  scale_x_discrete(labels=month.abb)
+  scale_x_discrete(labels=month.abb)+
+  theme(plot.title = element_text(hjust = 0.5))
 
 
 
@@ -108,12 +110,13 @@ plot3a = ggplot(aggregated_by_month, aes(as.factor(MONTH) , DEPARTURE_DELAY)) +
 aggregated_by_month = aggregate(ARRIVAL_DELAY ~ MONTH, cleanedData, mean)
 month_num = as.numeric(cleanedData$MONTH)
 plot3b = ggplot(aggregated_by_month, aes(as.factor(MONTH) , ARRIVAL_DELAY)) +
-  geom_col(size=2,fill = "blue") +
+  geom_col(size=1, col="goldenrod",fill = " goldenrod1", alpha = .7) +
   theme_bw() +
   ylim(c(-1,15))+
-  ggtitle("Average delay in destination airports")+
+  ggtitle("Average arival delay by month")+
   scale_x_discrete(labels=month.abb)+
-  labs(x="Month", y="Arrival delay (min)") 
+  labs(x="Month", y="Arrival delay (min)")+
+  theme(plot.title = element_text(hjust = 0.5))
 
 library(gridExtra)
 grid.arrange(plot3a,plot3b,ncol=2)
@@ -123,23 +126,66 @@ grid.arrange(plot3a,plot3b,ncol=2)
 aggregated_by_WEEkday = aggregate(DEPARTURE_DELAY ~ DAY_OF_WEEK, cleanedData, mean)
 
 plot4a = ggplot(aggregated_by_WEEkday, aes(as.factor(DAY_OF_WEEK), DEPARTURE_DELAY)) +
-  geom_col(size=2,fill = "green") +
+  geom_col(size=1, col="blue",fill = " dark blue", alpha = .7) +
   theme_bw() +
   ylim(c(-1,15))+
-  ggtitle("Average delay in origin airports divided by weekday")+
+  ggtitle("Average departure delay by weekday")+
   labs(x="Day of week", y="Departure delay (min)") +
-  scale_x_discrete(breaks=1:7,labels=c("Mon","Tue","Wed","Thu","Fri","Sat","Sun"))
+  scale_x_discrete(breaks=1:7,labels=c("Mon","Tue","Wed","Thu","Fri","Sat","Sun"))+
+  theme(plot.title = element_text(hjust = 0.5))
 
 #Plot 4b.  WEEkday - delay DESTINATION
 
 aggregated_by_WEEkday = aggregate(ARRIVAL_DELAY ~ DAY_OF_WEEK, cleanedData, mean)
 
 plot4b = ggplot(aggregated_by_WEEkday, aes(as.factor(DAY_OF_WEEK), ARRIVAL_DELAY)) +
-  geom_col(size=2,fill = "green") +
+  geom_col(size=1, col="goldenrod",fill = " goldenrod1", alpha = .7) +
   theme_bw() +
   ylim(c(-1,15))+
-  ggtitle("Average delay in destination airports divided by weekday")+
+  ggtitle("Average arrival delay by weekday")+
   labs(x="Day of week", y="Departure delay (min)") +
-  scale_x_discrete(breaks=1:7,labels=c("Mon","Tue","Wed","Thu","Fri","Sat","Sun"))
+  scale_x_discrete(breaks=1:7,labels=c("Mon","Tue","Wed","Thu","Fri","Sat","Sun"))+
+  theme(plot.title = element_text(hjust = 0.5))
 
 grid.arrange(plot4a,plot4b,ncol=2)
+
+#clean date time and convert it to the right format
+parse_time = function(x) {
+  fixed_time = ifelse(nchar(x)[1]==3,paste0("0",x),x)
+  t2 <- format(strptime(fixed_time, format = "%H"), "%H")
+  return(t2)
+}
+
+aggregateByHour <- aggregate(DEPARTURE_DELAY ~ SCHEDULED_DEPARTURE, cleanedData, mean)
+
+aggregateByHour$SCHEDULED_DEPARTURE <- sapply(aggregateByHour$SCHEDULED_DEPARTURE, parse_time)
+
+aggregateByHour$SCHEDULED_DEPARTURE[is.na(aggregateByHour$SCHEDULED_DEPARTURE)] <-'00'
+
+aggregateByHour <- aggregate(DEPARTURE_DELAY ~ SCHEDULED_DEPARTURE, aggregateByHour, mean)
+
+plot5a = ggplot(aggregateByHour, aes(as.factor(SCHEDULED_DEPARTURE), DEPARTURE_DELAY)) +
+  geom_col(size=1,fill = " dark blue", alpha = .7) +
+  theme_bw() +
+  ylim(c(0,18))+
+  ggtitle("Average departure delay by hour")+
+  labs(x="Hour", y="Departure delay (min)") +
+  theme(plot.title = element_text(hjust = 0.5))
+
+
+aggregateByHourARRIVAL <- aggregate(ARRIVAL_DELAY ~ SCHEDULED_ARRIVAL, cleanedData, mean)
+
+aggregateByHourARRIVAL$SCHEDULED_ARRIVAL <- sapply(aggregateByHourARRIVAL$SCHEDULED_ARRIVAL, parse_time)
+
+aggregateByHourARRIVAL$SCHEDULED_ARRIVAL[is.na(aggregateByHourARRIVAL$SCHEDULED_ARRIVAL)] <-'00'
+
+aggregateByHourARRIVAL <- aggregate(ARRIVAL_DELAY ~ SCHEDULED_ARRIVAL, aggregateByHourARRIVAL, mean)
+
+
+plot5b = ggplot(aggregateByHourARRIVAL, aes(as.factor(SCHEDULED_ARRIVAL), ARRIVAL_DELAY)) +
+  geom_col(size=1,fill = "goldenrod1", alpha = .7) +
+  theme_bw() +
+  ylim(c(-1.25,10))+
+  ggtitle("Average arrival delay by hour")+
+  labs(x="Hour", y="Arrival delay (min)") +
+  theme(plot.title = element_text(hjust = 0.5))
