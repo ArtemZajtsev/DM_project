d1a34e5b2d5285fb530e498e807d003770260876
rybachello <mykolarybachello@gmail.com>
2017-05-24 23:19:36 +0300

    Fix the conflict

--- project.R
+++ project.R
@@ -213,3 +213,184 @@ plot6b = ggplot(aggregateByAirlinesDeparture, aes(as.factor(AIRLINE), DEPARTURE_
   theme(plot.title = element_text(hjust = 0.5))
 
 grid.arrange(plot6a,plot6b,ncol=2)
+
+# Classification
+library(randomForest)
+library(gbm)
+library(dplyr)
+library(readr)
+cleanedData <- read_csv("D:/study/DM/DM_project/cleanedData.csv")
+cleanedData = cleanedData[!is.na(cleanedData$ARRIVAL_DELAY), ]
+isDelayed = function(t)
+{
+  ifelse(t > 15, 1, 0)
+}
+cleanedData = mutate(cleanedData, isDelayed  = isDelayed(cleanedData$ARRIVAL_DELAY))
+jfk_dataset = filter(cleanedData, cleanedData$ORIGIN_AIRPORT %in% c("JFK"))
+mia_dataset = filter(cleanedData, cleanedData$ORIGIN_AIRPORT %in% c("MIA"))
+san_dataset = filter(cleanedData, cleanedData$ORIGIN_AIRPORT %in% c("SAN"))
+atl_dataset = filter(cleanedData, cleanedData$ORIGIN_AIRPORT %in% c("ATL"))
+rw_nmb_jfk = nrow(jfk_dataset)
+rw_nmb_mia = nrow(mia_dataset)
+rw_nmb_san = nrow(san_dataset)
+rw_nmb_atl = nrow(atl_dataset)
+
+jfk_train_data = jfk_dataset[1:(rw_nmb_jfk * 0.8),]
+jfk_test_data = jfk_dataset [(rw_nmb_jfk * 0.8):rw_nmb_jfk,]
+
+mia_train_data = mia_dataset[1:(rw_nmb_mia * 0.8),]
+mia_test_data = mia_dataset [(rw_nmb_mia * 0.8):rw_nmb_mia,]
+
+san_train_data = san_dataset[1:(rw_nmb_san * 0.8),]
+san_test_data = san_dataset [(rw_nmb_san * 0.8):rw_nmb_san,]
+
+atl_train_data = atl_dataset[1:(rw_nmb_atl * 0.8),]
+atl_test_data = atl_dataset [(rw_nmb_atl * 0.8):rw_nmb_atl,]
+
+library(data.table)
+library(dplyr)
+library(party)
+library(readr)
+library(rpart)
+model_jfk <-rpart(as.factor(isDelayed)~
+                   MONTH + 
+                   DAY +
+                   DAY_OF_WEEK +
+                   AIRLINE +
+                   ORIGIN_AIRPORT +
+                   DEPARTURE_TIME +
+                   AIR_TIME +
+                   DESTINATION_AIRPORT +
+                   SCHEDULED_DEPARTURE +
+                   DISTANCE + 
+                   SCHEDULED_ARRIVAL
+                   ,data = jfk_train_data,control = rpart.control(misplit = 60,cp = 0))
+
+model_mia <-rpart(as.factor(isDelayed)~
+                    MONTH + 
+                    DAY +
+                    DAY_OF_WEEK +
+                    AIRLINE +
+                    ORIGIN_AIRPORT +
+                    DEPARTURE_TIME +
+                    AIR_TIME +
+                    SCHEDULED_DEPARTURE +
+                    DISTANCE + 
+                    SCHEDULED_ARRIVAL
+                  ,data = mia_train_data,control = rpart.control(misplit = 60,cp = 0))
+model_san <-rpart(as.factor(isDelayed)~
+                    MONTH + 
+                    DAY +
+                    DAY_OF_WEEK +
+                    AIRLINE +
+                    ORIGIN_AIRPORT +
+                    DEPARTURE_TIME +
+                    AIR_TIME +
+                    SCHEDULED_DEPARTURE +
+                    DISTANCE + 
+                    SCHEDULED_ARRIVAL
+                  ,data = san_train_data,control = rpart.control(misplit = 60,cp = 0))
+
+model_atl <-rpart(as.factor(isDelayed)~
+                    MONTH + 
+                    DAY +
+                    DAY_OF_WEEK +
+                    AIRLINE +
+                    ORIGIN_AIRPORT +
+                    DEPARTURE_TIME +
+                    AIR_TIME +
+                    SCHEDULED_DEPARTURE +
+                    DISTANCE + 
+                    SCHEDULED_ARRIVAL
+                  ,data = atl_train_data,control = rpart.control(misplit = 60,cp = 0))
+
+predict_atl = predict(model_atl, newdata = atl_test_data, type = "class")
+predict_san = predict(model_san, newdata = san_test_data, type = "class")
+
+predict_mia = predict(model_mia, newdata = mia_test_data, type = "class")
+
+predict_jfk = predict(model_jfk, newdata = jfk_test_data, type = "class")
+
+library(hydroGOF)
+library(ROCR)
+
+drawROCR = function(prd,testdataset)
+{
+  predictions = c(prd)
+  pred=prediction(predictions,testdataset$isDelayed)
+  perf_AUC=performance(pred,"auc")
+  AUC=perf_AUC@y.values[[1]]
+  perf_ROC=performance(pred,"tpr","fpr")
+  perf_ROC
+  
+}
+
+jfk = drawROCR(predict_jfk,jfk_test_data)
+atl = drawROCR(predict_atl,atl_test_data)
+san = drawROCR(predict_san,san_test_data)
+mia = drawROCR(predict_mia,mia_test_data)
+
+plotCombine = function(a,b,c,d)
+{
+  plot(a, col = "green",main ="ROC Curve",lwd = 2)
+  plot(b, col = "blue",add = TRUE,lwd = 2)
+  plot(c, col = "purple",add = TRUE,lwd = 2)
+  plot(d, col = "pink",add = TRUE,lwd = 2)
+  legend("bottomright", c("JFK: 0.758","MIA: 0.830","SAN: 0.852","ATL: 0.860"), lty=c(1,1), lwd=c(2.5,2.5),col=c("green","blue","purple","pink"))
+}
+plotCombine(jfk,mia,san,atl)
+
+predictions=c(predict_jfk)
+pred=prediction(predictions,jfk_test_data$isDelayed)
+perf_AUC=performance(pred,"auc")
+AUC=perf_AUC@y.values[[1]]
+
+perf_ROC=performance(pred,"tpr","fpr")
+plot(perf_ROC, main="ROC plot")
+text(0.5,0.5,paste("AUC = ",format(AUC, digits=8, scientific=FALSE)))
+
+
+rmse(test_data$isDelayed,as.numeric(predict_dt))
+predict_dt
+str(predict_dt)
+str(test_data$isDelayed)
+write.csv(newdataset,file='dataset.csv',quote = FALSE,row.names = TRUE)
+
+
+summary(newdataset)
+
+
+rw_nmb = nrow(cleanedData) # 80% of dataset 
+test_data = cleanedData[(rw_nmb * 0.8):rw_nmb,]
+
+train_data = select(cleanedData[1:(rw_nmb * 0.8),] #select 80% of data set
+                    ,MONTH
+                    ,DAY
+                    ,DAY_OF_WEEK
+                    ,AIRLINE
+                    ,ORIGIN_AIRPORT
+                    ,DESTINATION_AIRPORT
+                    ,SCHEDULED_DEPARTURE
+                    ,SCHEDULED_ARRIVAL
+                    ,isDelayed
+                   
+)
+
+#summary(is.na(train_data$ARRIVAL_DELAY))
+
+write.csv(train_data,file='trainData.csv',quote = FALSE,row.names = TRUE)
+
+ntrees  = 6
+set.seed(6)
+
+model = gbm.fit(
+  x = train_data
+  , y = isDelayed,
+  distribution = "gaussian",
+  n.trees = ntrees,
+  shrinkage = 0.01,
+  interaction.depth = 3,
+  n.minobsinnode = 12,
+  nTrain = round(rw_nmb * 0.8*0.8),
+  verbose = TRUE.
+)
